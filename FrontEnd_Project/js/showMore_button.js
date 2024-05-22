a5b798f358d8d0dff35262f44a8b10ca5de0cd86
choieuihyun <amammmaa1@naver.com>
2024-05-22 17:18:26 +0900

    프로젝트 경로 변경

--- FrontEnd_Project/js/showMore_button.js
+++ FrontEnd_Project/js/showMore_button.js
@@ -1,8 +1,3 @@
-// "더보기" 버튼을 클릭하면 숨겨진 이미지를 선택 (.image-container img.hidden)합니다.
-// 각 숨겨진 이미지에서 hidden 클래스를 제거하여 이미지를 표시합니다.
-// "더보기" 버튼의 텍스트를 "이미지 로드 완료"로 바꾸고, 버튼을 비활성화 (disabled)합니다.
-
-
 const showMoreButton = document.querySelector('.show_more_button');
 const hiddenImages = document.querySelectorAll('.image_list_container .hidden_image');
 
